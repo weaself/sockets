f7988c6b7648e3bd313bafbd7b6591f98604f25d
adrian.wisla <adrwisla@gmail.com>
2018-02-05 22:33:06 +0000

    new

--- ServerListenerApp.java
+++ ServerListenerApp.java
@@ -6,26 +6,19 @@ import java.net.Socket;
 
 public class ServerListenerApp {
     ServerSocket serSocket;
-    Socket fromClientSocket;
     int portToListenOn = 8999;
     boolean keepRunning = true;
 
     public ServerListenerApp() {
         try {
             ServerSocket serSocket = new ServerSocket(portToListenOn);
-        } catch(IOException ioex) {
-            ioex.printStackTrace();
-        }
-
-        while(keepRunning) {
-            try {
-                fromClientSocket = serSocket.accept();
-
-            }catch (IOException ioex) {
-                System.out.println(fromClientSocket.toString());}
-
-            Thread firstServer = new Thread(new ServerListener(fromClientSocket));
-            firstServer.start();
+            while (keepRunning) {
+                Socket fromClientSocket = serSocket.accept();
+                Thread firstServer = new Thread(new ServerListener(fromClientSocket));
+                firstServer.start();
+            }
+            }catch(IOException ioex){
+                System.out.println("ioex");
         }
 
     }
